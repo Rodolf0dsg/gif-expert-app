211611e0f8de329748aa05c93317f04ce2cf3f2c
Rodolfo <fororus12@gmail.com>
2023-11-24 17:35:54 -0400

    Form aria-labe;

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -23,7 +23,7 @@ export const AddCategory = ({ onNewCategory }) => {
 
 
     return (
-        <form onSubmit={ onSubmit }>
+        <form onSubmit={ onSubmit } aria-label="form">
             <input
                 type="text" 
                 placeholder="Buscar un Gif"
