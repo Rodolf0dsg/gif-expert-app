8b921ba17387cae5b233794bcbe829c506949e46
Rodolfo <fororus12@gmail.com>
2025-08-22 17:38:15 -0400

    Try/Catch

--- src/gifs/actions/get-gifs-by-query.action.ts
+++ src/gifs/actions/get-gifs-by-query.action.ts
@@ -3,19 +3,29 @@ import { Gif } from "../interfaces/gif.interface";
 import { giphtApi } from "../api/gipht.api";
 
 export const getGifsByQuery = async( query: string ): Promise<Gif[]> => {
-    const response = await giphtApi<GiphyResponse>('/search', {
-        params: {
-            q: query,
-            limit: 12,
-        }
-    });
 
-    return response.data.data.map( gif => ({
-        id:     gif.id,
-        title:  gif.title,
-        url:    gif.images.original.url,
-        width:  Number(gif.images.original.width),
-        height: Number(gif.images.original.height),
-    }));
+    if ( query.trim().length === 0 ){
+        return [];
+    }
 
+    try {
+        const response = await giphtApi<GiphyResponse>('/search', {
+            params: {
+                q: query,
+                limit: 12,
+            }
+        });
+        
+        return response.data.data.map( gif => ({
+            id:     gif.id,
+            title:  gif.title,
+            url:    gif.images.original.url,
+            width:  Number(gif.images.original.width),
+            height: Number(gif.images.original.height),
+        })); 
+
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 };
