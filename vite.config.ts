c43ce9d8ca8a9ace811f24128d9cdca07a52855f
Rodolfo <fororus12@gmail.com>
2025-08-24 16:31:43 -0400

    base added for bundled version

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
   base: '/gif-expert-app/',
+
 })
